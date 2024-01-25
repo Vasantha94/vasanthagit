0633c076cc1981de48e34eecb8908ee06d197e6f
vasantha94 <vasantha94.qa@gmail.com>
2024-01-25 08:34:41 +0200

    second file

---
+++ test2.py
@@ -0,0 +1,67 @@
+print("Integer Tasks")
+print("Addition 2+5:'", 2+5)
+print("Substraction 3-1:", 3-1)
+print("Multiplication 3*4:", 3*4)
+print("Division 4/2:", 4/2)
+print("Percentage 10%3:", 10%3)
+
+print("String Tasks")
+a="hai"
+b="hello"
+print("Adding two string (a=hai, b=hello)", a+b)
+
+print("repeated_a")
+repeated_a = a * 3
+print(repeated_a)
+
+print("length_a")
+length_a = len(a)
+print(length_a)
+
+print("first_char_a")
+first_char_a = a[0]
+print(first_char_a)
+
+
+print("List Tasks")
+
+print("my_list = [1, 2, 3, 4, 5]")
+my_list = [1, 2, 3, 4, 5]
+
+first_element = my_list[0]
+print(first_element)
+
+sublist = my_list[1:4]
+print(sublist)  
+
+my_list.append(6)  
+print(my_list)  
+
+extension_list = [7, 8, 9]
+my_list.extend(extension_list)  
+print(my_list) 
+
+my_list.insert(2, 10)  
+print(my_list)  
+
+my_list.insert(2, 10)  
+print(my_list)  
+
+my_list.sort() 
+print(my_list) 
+
+my_list.reverse()  
+print(my_list)  
+
+print("Boolean tasks: a=5, b=10")
+
+a = 5
+b = 10
+
+print("equal_to =", a == b)         
+print("not_equal_to =", a != b)
+print("less_than =", a < b)        
+print("greater_than =", a > b)      
+print("less_than_equal =", a <= b )
+print("greater_than_equal =", a >= b )
+
